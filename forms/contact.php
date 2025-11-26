96fd2b87c7b733fac53c446a78c38ab7cacba6fb
Goutham lal <gouthamlal666@gmail.com>
2025-11-26 06:34:15 +0530

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,34 @@
+<?php
+
+  // Your email
+  $receiving_email_address = 'gouthamlal666@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  //  Enable SMTP for Gmail (Recommended)
+  $contact->smtp = array(
+    'host' => 'smtp.gmail.com',
+    'username' => 'gouthamlal666@gmail.com',
+    'password' => 'sgsx uwmb ymhc mkas', 
+    'port' => '587'
+  );
+
+  $contact->add_message($_POST['name'], 'From');
+  $contact->add_message($_POST['email'], 'Email');
+  isset($_POST['phone']) && $contact->add_message($_POST['phone'], 'Phone');
+  $contact->add_message($_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
